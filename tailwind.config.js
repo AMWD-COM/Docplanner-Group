ab970a6edeaf3e84891a0e63dcbd62e067f244ad
AMWD-COM <miziahmedwork@gmail.com>
2021-09-20 22:41:42 +0100

    Add HTML-CSS-JS file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: [
-    // "./public/index.html",
+    "./public/index.html",
     // "./public/main.js"
 ],
   darkMode: false, // or 'media' or 'class'
@@ -9,11 +9,24 @@ module.exports = {
       fontFamily: {
         'WorkSans': ['"Work Sans"'],
         'sans':['Roboto', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"']
+      },
+      screens: {
+        'xs': '480px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
       }
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      grayscale: ["hover"],
+      display: ['hover', 'focus', 'group-hover', 'group-focus'],
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
